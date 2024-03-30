8f4d496b1c0b2191a82a2419630fa6c25696dfda
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-03-30 10:40:13 +0100

    auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,8 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/current_user", () => {
+    res.send(req.user);
+  });
 };
