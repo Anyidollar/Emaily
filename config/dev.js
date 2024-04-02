b422e536ba79a43c85d8275b2cc388c950edbaf9
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-04-02 10:26:42 +0100

    new changes

--- config/dev.js
+++ config/dev.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   googleClientID:
-    "891989646149-brakv4tmhsrm7sl4dju9trph3p8epa0n.apps.googleusercontent.com",
-  googleClientSecret: "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",
+    "",
+  googleClientSecret: "",
   mongoURI:
-    "mongodb+srv://ifeanyionyekwere123:LFW0xk0Dv5rdWlxQ@cluster0.hy6tk0g.mongodb.net/emailydb?retryWrites=true&w=majority",
-  cookieKey: "dkjfhkshfslfhssjhkjsjbkfdlanjfdb,dnzc.sbdkls",
+    "",
+  cookieKey: "",
 };
