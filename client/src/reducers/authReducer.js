c2f2c6f68a938b077d7f9d4dc008aba722fb36f1
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-04-12 16:29:32 +0100

    new changes

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,5 @@
-/* eslint-disable import/no-anonymous-default-export */
 import { FETCH_USER } from "../actions/types";
-
-export default function (state = null, action) {
+function authReducer(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false;
@@ -9,3 +7,5 @@ export default function (state = null, action) {
       return state;
   }
 }
+
+export default authReducer;
